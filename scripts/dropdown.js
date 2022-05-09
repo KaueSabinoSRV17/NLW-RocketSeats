1716fe6e57c8950d93c7ea004a0a70839c021a57
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2022-05-08 22:16:55 -0300

    build: código com funçoes anonimas

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,4 +1,14 @@
-function botao() {
-    let menu = document.getElementById('menu')
+
+const menu = document.getElementById('menu')
+const button = document.getElementById('btnLinks')
+
+button.addEventListener('click', () => {
     menu.classList.toggle('menuExpanded')
-}
+    document.body.classList.toggle('menuExpanded')
+})
+
+menu.addEventListener('click', () => {
+    menu.classList.remove('menuExpanded')
+    document.body.classList.toggle('menuExpanded')
+})
+
