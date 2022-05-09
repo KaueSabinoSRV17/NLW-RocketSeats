d9ab0a5f1e4f0e339709f302569458dd4bdbe939
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2022-05-08 22:15:25 -0300

    feat: funções que checam pontos da tela

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -8,6 +8,9 @@ const header = document.getElementById('header')
 const backToTop = document.getElementById('btnTop')
 
 function scroll() {
+    changeUnderlineOnTheTopMenu(projetos)
+    changeUnderlineOnTheTopMenu(home)
+    changeUnderlineOnTheTopMenu(contatos)
     fixedHeader()    
     addBackToTopButton()
 }
@@ -18,4 +21,30 @@ function fixedHeader() {
 
 function addBackToTopButton() {
     scrollY >= 250 ? backToTop.classList.add('showButton') : backToTop.classList.remove('showButton')
+}
+
+// funcao que alterar a linha sublinada do menu
+
+function changeUnderlineOnTheTopMenu(section) {
+    const targetLine = scrollY + innerHeight / 2
+
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+
+    const sectionEndsAt = sectionTop + sectionHeight
+
+    const sectionTopReachedOrPassedTargetLine = targetLine >= sectionTop
+    const sectionEndReachedOrPassedTargetLine = sectionEndsAt <= targetLine
+
+    const insideSectionBoundaries = sectionTopReachedOrPassedTargetLine && !sectionEndReachedOrPassedTargetLine
+
+    const sectionID = section.getAttribute('id')
+
+    const link = document.querySelector(`a[href*=${sectionID}]`)
+
+    link.classList.remove('active')
+
+    if (insideSectionBoundaries) {
+        link.classList.add('active')
+    }
 }
